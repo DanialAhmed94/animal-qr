20ddc18d70b534dcafac00acbcd01192a5104f56
wasiqtayya <wasiqtayyab30@gmail.com>
2024-06-04 02:00:25 +0500

    add android notifciation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,64 @@
 
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show SystemChrome, SystemUiOverlayStyle;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:pet_project/app_theme.dart';
 import 'package:pet_project/views/home/home_view.dart';
 
 import 'package:pet_project/views/splash_view.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'local.notification.dart';
+
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseMessaging.instance.requestPermission();
+  if (Platform.isAndroid) {
+    await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  }
+  FirebaseMessaging.instance.getInitialMessage();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  final messaging = FirebaseMessaging.instance;
+
+  final settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  if (kDebugMode) {
+    print('Permission granted: ${settings.authorizationStatus}');
+  }
+  String? token = await messaging.getToken();
+  print('Registration Token=$token');
+
+
+  await LocalNotification.initialize(flutterLocalNotificationsPlugin);
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
@@ -17,8 +69,47 @@ void main() {
   runApp(const MyApp());
 }
 
+// Future<void> initializeNotificationSettings() async {
+//   String? token = await FirebaseMessaging.instance.getToken();
+//   print("FCM Token: $token");
+//   FirebaseMessaging.instance.getInitialMessage();
+//   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//   FlutterLocalNotificationsPlugin();
+//   await LocalNotification.initialize(flutterLocalNotificationsPlugin);
+//   FirebaseMessaging messaging = FirebaseMessaging.instance;
+//
+//   NotificationSettings settings = await messaging.requestPermission(
+//     alert: true,
+//     announcement: false,
+//     badge: true,
+//     carPlay: false,
+//     criticalAlert: false,
+//     provisional: false,
+//     sound: true,
+//   );
+//
+//   print('User granted permission: ${settings.authorizationStatus}');
+//
+//   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+//     alert: true, // Required to display a heads up notification
+//     badge: true,
+//     sound: true,
+//   );
+//   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+//     print('Got a message whilst in the foreground!');
+//     print('Message data: ${message.data}');
+//
+//     if (message.notification != null) {
+//       print('Message also contained a notification: ${message.notification!.body.toString()}');
+//     }
+//   });
+//
+// }
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
